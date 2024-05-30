edfadd29ed8280b282e778431406ebb8ae83a54a
Tom Beerbower <thomas@techelevator.com>
2024-05-30 11:48:08 -0400

    added documentation for LeaderboardMenu class (#17)

--- src/main/java/org/tbeerbower/LeaderboardMenu.java
+++ src/main/java/org/tbeerbower/LeaderboardMenu.java
@@ -7,10 +7,14 @@ import org.tbeerbower.view.View;
 import static org.tbeerbower.model.PlayerComparator.Mode.AVG_GUESSES_ASC;
 import static org.tbeerbower.model.PlayerComparator.Mode.WINS_DESC;
 
+/**
+ * Sub menu class for the leaderboard options of the TErdle game.
+ */
 public class LeaderboardMenu extends Menu<LeaderboardMenu.Option> {
 
     public static final Option[] OPTIONS_VALUES = Option.values();
 
+    // Enum for leaderboard menu options
     public enum Option {
         BY_GAMES_WON("Leaderboard by games won"),
         BY_AVG_GUESSES("Leaderboard by average guesses"),
@@ -28,10 +32,20 @@ public class LeaderboardMenu extends Menu<LeaderboardMenu.Option> {
         }
     }
 
+
+    // ***** Constructors *****************************************************
+
     public LeaderboardMenu(View view) {
         super(OPTIONS_VALUES, view);
     }
 
+
+    // ***** LeaderboardMenu **************************************************
+
+    /**
+     * Run the menu and show the options, waiting for a user selection.
+     * @param playersService  the service used to display the player leaderboards
+     */
     public void run(PlayersService playersService) {
         boolean run = true;
 
